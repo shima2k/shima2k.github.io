a2fd8c4a09a469a39940eab610e4f8d782817b3e
Nhóm 11 <94359113+shima2k@users.noreply.github.com>
2024-11-15 16:47:53 +0700

    Add files via upload

---
+++ hack.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main1.css">
+</head>
+<body >
+<table bgcolor="green">
+    <tr>
+        <td id="test"></td>
+        <td id="test1"></td>
+        <td id="test2"></td>
+        <td id="test3"></td>
+        <td id="test4"></td>
+</tr>
+<tr>
+        <td id="test5"></td>
+        <td id="test6"></td>
+        <td id="test7"></td>
+        <td id="test8"></td>
+        <td id="test9"></td>
+        </tr>
+<tr>
+        <td id="test10"></td>
+        <td id="test11"></td>
+        <td id="test12"></td>
+        <td id="test13"></td>
+        <td id="test14"></td>
+        </tr>
+<tr>
+        <td id="test15"></td>
+        <td id="test16"></td>
+        <td id="test17"></td>
+        <td id="test18"></td>
+        <td id="test19"></td>
+        </tr>
+<tr>
+        <td id="test20"></td>
+        <td id="test21"></td>
+        <td id="test22"></td>
+        <td id="test23"></td>
+        <td id="test24"></td>
+        </tr>
+<tr>
+        <td id="test25"></td>
+        <td id="test26"></td>
+        <td id="test27"></td>
+        <td id="test28"></td>
+        <td id="test29"></td>
+        </tr>
+<tr>
+        <td id="test30"></td>
+        <td id="test31"></td>
+        <td id="test32"></td>
+        <td id="test33"></td>
+        <td id="test34"></td>
+        </tr>
+<tr>
+        <td id="test35"></td>
+        <td id="test36"></td>
+        <td id="test37"></td>
+        <td id="test38"></td>
+        <td id="test39"></td>
+    </tr>
+    
+    <tr>
+        <td id="test40"></td>
+        <td id="test41"></td>
+        <td id="test42"></td>
+        <td id="test43"></td>
+        <td id="test44"></td>
+        </tr>
+<tr>
+        <td id="test45"></td>
+        <td id="test46"></td>
+        <td id="test47"></td>
+        <td id="test48"></td>
+        <td id="test49"></td>
+        </tr>
+<tr>
+        <td id="test50"></td>
+        <td id="test51"></td>
+        <td id="test52"></td>
+        <td id="test53"></td>
+        <td id="test54"></td>
+        </tr>
+<tr>
+        <td id="test55"></td>
+        <td id="test56"></td>
+        <td id="test57"></td>
+        <td id="test58"></td>
+        <td id="test59"></td>
+        </tr>
+<tr>
+        <td id="test60"></td>
+        <td id="test61"></td>
+        <td id="test62"></td>
+        <td id="test63"></td>
+        <td id="test64"></td>
+        </tr>
+<tr>
+        <td id="test65"></td>
+        <td id="test66"></td>
+        <td id="test67"></td>
+        <td id="test68"></td>
+        <td id="test69"></td>
+        </tr>
+
+
+</table>
+
+</body>
+<script src="js\main1.js"></script>
+</html>
